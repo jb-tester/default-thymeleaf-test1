a3927f951000cd84afde13741d42f4e6a31c7973
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2017-08-30 15:28:34 +0300

    Initial commit

---
+++ src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest1/DefaultThymeleafTest1Application.java
@@ -0,0 +1,12 @@
+package com.mytests.springboot.webapp.defaults.thymeleaf.defaultthymeleaftest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultThymeleafTest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefaultThymeleafTest1Application.class, args);
+	}
+}
---
+++ src/main/java/com/mytests/springboot/webapp/defaults/thymeleaf/defaultthymeleaftest1/MyController.java
@@ -0,0 +1,21 @@
+package com.mytests.springboot.webapp.defaults.thymeleaf.defaultthymeleaftest1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 8/14/2017.
+ * Project: default-thymeleaf-test0
+ * *******************************
+ */
+@Controller
+public class MyController {
+    @RequestMapping("/")
+    public String Method(ModelMap model) {
+        model.addAttribute("home_attr1", "test1");
+        model.addAttribute("home_attr2", "this is default thymeleaf configuration test with thymeleaf suffix and prefix set via application properties");
+        return "home";
+    }
+}
